3db2117e1f77ae10627fc6ea2dd89551c262b88e
Ahaan Soni <kartiksoni0008@gmail.com>
2023-12-23 19:32:51 +0530

    INFO - Custome react beginning

---
+++ customReact/index.js
@@ -0,0 +1,14 @@
+function customRender(reactElement, root) {}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit Google",
+};
+
+const root = document.getElementById("id");
+
+customRender(reactElement, root);
